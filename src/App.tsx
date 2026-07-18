b430b2f1c1500feb56d5539b9e8551da1cbdb20a
gurudhiman75-hash <gurudhiman75@gmail.com>
2026-07-18 23:52:34 +0530

    feat(question-studio): route to review cockpit

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ import { TestDetailPage } from '@/pages/tests/TestDetailPage';
 import { TestsPage } from '@/pages/tests/TestsPage';
 
 const QuestionStudioPage = lazy(() =>
-  import('@/pages/content/QuestionStudioLivePage').then((module) => ({ default: module.QuestionStudioLivePage })),
+  import('@/pages/content/QuestionStudioCockpitPage').then((module) => ({ default: module.QuestionStudioCockpitPage })),
 );
 const TestBuilderPage = lazy(() =>
   import('@/pages/tests/TestBuilderPage').then((module) => ({ default: module.TestBuilderPage })),
