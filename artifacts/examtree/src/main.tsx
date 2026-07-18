bf0822e363e6be35412b214c3bbfa7838e035a35
gurudhiman75-hash <gurudhiman75@gmail.com>
2026-07-18 15:02:05 +0530

    fix(result): install non-blocking local storage

--- artifacts/examtree/src/main.tsx
+++ artifacts/examtree/src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import "./lib/install-safe-storage";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
