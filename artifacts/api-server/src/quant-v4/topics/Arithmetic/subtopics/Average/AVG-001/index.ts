93bfb872a76c15f672e2adf23a9ebf19922764f6
gurudhiman75-hash <gurudhiman75@gmail.com>
2026-07-23 17:01:20 +0530

    feat(avg): export Question Studio adapter

--- artifacts/api-server/src/quant-v4/topics/Arithmetic/subtopics/Average/AVG-001/index.ts
+++ artifacts/api-server/src/quant-v4/topics/Arithmetic/subtopics/Average/AVG-001/index.ts
@@ -2,3 +2,4 @@ export * from "./foundation/types";
 export * from "./foundation/library";
 export * from "./foundation/math";
 export * from "./foundation/pipeline";
+export * from "./question-studio-adapter";
