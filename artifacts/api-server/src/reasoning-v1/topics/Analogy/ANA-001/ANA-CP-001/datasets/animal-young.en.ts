825e1ae0caeb5f4d7f8d7aaabb7a85689d3e6db6
gurudhiman75-hash <gurudhiman75@gmail.com>
2026-07-24 16:21:32 +0530

    refactor(reasoning): split animal-young dataset

---
+++ artifacts/api-server/src/reasoning-v1/topics/Analogy/ANA-001/ANA-CP-001/datasets/animal-young.en.ts
@@ -0,0 +1,2 @@
+import type { SemanticPair } from "./types";
+export const ANIMAL_YOUNG_PAIRS: readonly SemanticPair[] = [["Lion","Cub"],["Cow","Calf"],["Dog","Puppy"],["Horse","Foal"],["Cat","Kitten"],["Goat","Kid"],["Sheep","Lamb"],["Deer","Fawn"],["Duck","Duckling"],["Hen","Chick"],["Pig","Piglet"],["Kangaroo","Joey"]];
