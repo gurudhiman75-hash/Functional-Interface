ad02c8b15e5c7d28b20118097478e88175314bb7
gurudhiman75-hash <gurudhiman75@gmail.com>
2026-07-24 16:21:43 +0530

    refactor(reasoning): split male-female dataset

---
+++ artifacts/api-server/src/reasoning-v1/topics/Analogy/ANA-001/ANA-CP-001/datasets/male-female.en.ts
@@ -0,0 +1,2 @@
+import type { SemanticPair } from "./types";
+export const MALE_FEMALE_PAIRS: readonly SemanticPair[] = [["Horse","Mare"],["Lion","Lioness"],["Peacock","Peahen"],["Ram","Ewe"],["Bull","Cow"],["Stallion","Mare"],["Tiger","Tigress"],["Fox","Vixen"],["Cock","Hen"],["Drake","Duck"],["Boar","Sow"],["Gander","Goose"]];
