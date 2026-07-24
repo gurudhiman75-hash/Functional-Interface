51f77ad9ebf3b6290f00581251d2c49052fa898f
gurudhiman75-hash <gurudhiman75@gmail.com>
2026-07-24 07:39:24 +0530

    feat(question-studio): mount profile calibration workspace

--- artifacts/admin-app/src/pages/content/QuestionStudioOperationsPage.tsx
+++ artifacts/admin-app/src/pages/content/QuestionStudioOperationsPage.tsx
@@ -1,12 +1,14 @@
 import { QuestionStudioCockpitPage } from './QuestionStudioCockpitPage';
 import { QuestionStudioDifficultyMixControls } from './QuestionStudioDifficultyMixControls';
 import { QuestionStudioExamProfileSummary } from './QuestionStudioExamProfileSummary';
+import { QuestionStudioProfileCalibration } from './QuestionStudioProfileCalibration';
 import { QuestionStudioRecoveryDock } from './QuestionStudioRecoveryDock';
 
 export function QuestionStudioOperationsPage() {
   return (
     <>
       <QuestionStudioExamProfileSummary />
+      <QuestionStudioProfileCalibration />
       <QuestionStudioDifficultyMixControls />
       <QuestionStudioCockpitPage />
       <QuestionStudioRecoveryDock />
