a54adb40f6b96596eebb832728ca8201f12754eb
gurudhiman75-hash <gurudhiman75@gmail.com>
2026-07-24 06:50:54 +0530

    Mount Question Studio mix controls

--- artifacts/admin-app/src/pages/content/QuestionStudioOperationsPage.tsx
+++ artifacts/admin-app/src/pages/content/QuestionStudioOperationsPage.tsx
@@ -1,9 +1,11 @@
 import { QuestionStudioCockpitPage } from './QuestionStudioCockpitPage';
+import { QuestionStudioDifficultyMixControls } from './QuestionStudioDifficultyMixControls';
 import { QuestionStudioRecoveryDock } from './QuestionStudioRecoveryDock';
 
 export function QuestionStudioOperationsPage() {
   return (
     <>
+      <QuestionStudioDifficultyMixControls />
       <QuestionStudioCockpitPage />
       <QuestionStudioRecoveryDock />
     </>
