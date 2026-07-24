a1b927a804377ce095cd17f6a43beea86e009ecc
gurudhiman75-hash <gurudhiman75@gmail.com>
2026-07-24 12:44:00 +0530

    feat(pnc): compose CP-004 human-owned libraries

--- artifacts/api-server/src/quant-v4/topics/Arithmetic/subtopics/PermutationAndCombination/PNC-001/foundation/library.ts
+++ artifacts/api-server/src/quant-v4/topics/Arithmetic/subtopics/PermutationAndCombination/PNC-001/foundation/library.ts
@@ -1,7 +1,9 @@
 import questionLanguageBase from "../question-language.en.json";
 import questionLanguageCp003 from "../question-language.cp003.en.json";
+import questionLanguageCp004 from "../question-language.cp004.en.json";
 import taskRegistryBase from "../task-registry.library.json";
 import taskRegistryCp003 from "../task-registry.cp003.library.json";
+import taskRegistryCp004 from "../task-registry.cp004.library.json";
 import variableRanges from "../variable-ranges.library.json";
 import constraintProfiles from "../constraint-profiles.library.json";
 import explanationLibrary from "../explanation.en.json";
@@ -22,20 +24,24 @@ type VariableRanges = {
     factorial: number[]; factorialGap: number[];
     permutationN: number[]; permutationR: number[];
     combinationN: number[]; combinationR: number[];
+    multisetTotal: number[]; multisetRepeat: number[];
   }>;
   generation: {
     maxAttempts: number; minimumAnswer: number; preferDistinctStageCounts: boolean;
     maximumFactorialArgument: number; maximumPermutationObjects: number; maximumCombinationObjects: number;
+    maximumMultisetObjects: number; maximumMultisetMultiplicity: number;
   };
 };
 
 const qlEntries = [
   ...(questionLanguageBase.entries as Pnc001QuestionLanguageEntry[]),
   ...(questionLanguageCp003.entries as Pnc001QuestionLanguageEntry[]),
+  ...(questionLanguageCp004.entries as Pnc001QuestionLanguageEntry[]),
 ];
 const registryGroups = [
   ...(taskRegistryBase.groups as Pnc001RegistryGroup[]),
   ...(taskRegistryCp003.groups as Pnc001RegistryGroup[]),
+  ...(taskRegistryCp004.groups as Pnc001RegistryGroup[]),
 ];
 const qlById = new Map(qlEntries.map((entry) => [entry.qlId, entry]));
 const expandedEntries: Pnc001QuestionEntry[] = registryGroups.flatMap((group) => group.qlIds.map((qlId) => {
