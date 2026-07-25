c6d3028445c761ff77a41a3b64837015d176784c
gurudhiman75-hash <gurudhiman75@gmail.com>
2026-07-25 12:02:29 +0530

    feat(reasoning): expose COD-CP-004 runtime

--- artifacts/api-server/src/reasoning-v1/topics/Coding-Decoding/COD-001/index.ts
+++ artifacts/api-server/src/reasoning-v1/topics/Coding-Decoding/COD-001/index.ts
@@ -2,3 +2,4 @@ export * from "./foundation/types";
 export * from "./COD-CP-001/index";
 export * from "./COD-CP-002/index";
 export * from "./COD-CP-003/index";
+export * from "./COD-CP-004/index";
