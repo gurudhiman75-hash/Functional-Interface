80c54ebbc2729d910a1e8f894b517213fa2d3002
gurudhiman75-hash <gurudhiman75@gmail.com>
2026-07-25 09:05:45 +0530

    feat(reasoning): add ANA-CP-004 task registry

---
+++ artifacts/api-server/src/reasoning-v1/topics/Analogy/ANA-001/ANA-CP-004/task-registry.ts
@@ -0,0 +1,5 @@
+export { ANA_CP004_QLS } from "./question-language.en";
+export { ANA_CP004_RULES, setRuleById } from "./rule-definitions";
+export { generateSetAnalogy } from "./generator";
+export { solveSetRule, verifySetTransfer, matchingSetRules } from "./independent-solver";
+export { checkSetAmbiguity } from "./ambiguity-checker";
