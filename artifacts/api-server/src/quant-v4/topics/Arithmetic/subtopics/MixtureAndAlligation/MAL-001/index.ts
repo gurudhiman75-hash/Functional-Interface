643b6b6c42649ea082f9f7ddba3a055c8522532c
gurudhiman75-hash <gurudhiman75@gmail.com>
2026-07-27 22:50:54 +0530

    feat(mal): export CP001 discovery classification

--- artifacts/api-server/src/quant-v4/topics/Arithmetic/subtopics/MixtureAndAlligation/MAL-001/index.ts
+++ artifacts/api-server/src/quant-v4/topics/Arithmetic/subtopics/MixtureAndAlligation/MAL-001/index.ts
@@ -1,4 +1,9 @@
 export { MAL_CP001_PROTOTYPE_REGISTRY, getMalCp001PrototypeEntry } from "./foundation/cp001-registry";
+export {
+  MAL_CP001_CANDIDATE_CONTRACT_IDS,
+  MAL_CP001_DISCOVERY_CLASSIFICATION,
+  getMalCp001DiscoveryClassification,
+} from "./foundation/cp001-discovery-classification";
 export { generateMalCp001PrototypeParameters } from "./foundation/cp001-parameter-generator";
 export { generateMalCp001Prototype } from "./foundation/pipeline";
 export { solveMalCp001 } from "./foundation/solver";
