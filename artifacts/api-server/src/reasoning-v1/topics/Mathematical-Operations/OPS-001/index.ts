dee2a97f9dbf5e74f10eed54213ce4d22f1ebfd0
gurudhiman75-hash <gurudhiman75@gmail.com>
2026-07-27 11:02:04 +0530

    feat(ops-001): expose frozen chapter integration surface

---
+++ artifacts/api-server/src/reasoning-v1/topics/Mathematical-Operations/OPS-001/index.ts
@@ -0,0 +1,14 @@
+export * from "./registry";
+export * from "./runtime";
+export * from "./delivery-adapter";
+export * from "./analytics";
+
+export { OPS_CP_001_RUNTIME } from "./OPS-CP-001";
+export { OPS_CP_002_RUNTIME } from "./OPS-CP-002";
+export { OPS_CP_003_RUNTIME } from "./OPS-CP-003";
+export { OPS_CP_004_RUNTIME } from "./OPS-CP-004";
+export { OPS_CP_005_RUNTIME } from "./OPS-CP-005";
+export { OPS_CP_006_RUNTIME } from "./OPS-CP-006";
+export { OPS_CP_007_RUNTIME } from "./OPS-CP-007";
+export { OPS_CP_008_RUNTIME } from "./OPS-CP-008";
+export { OPS_CP_009_RUNTIME } from "./OPS-CP-009";
