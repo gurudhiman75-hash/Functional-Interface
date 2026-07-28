c881ea9ede9e5a424cdda495cf0dae256e106c0a
gurudhiman75-hash <gurudhiman75@gmail.com>
2026-07-28 08:12:52 +0530

    feat(blr): export initial BLR runtime

---
+++ artifacts/api-server/src/reasoning-v1/topics/Blood-Relations/BLR-001/index.ts
@@ -0,0 +1,7 @@
+export * from "./foundation/types";
+export * from "./foundation/family-validity";
+export * from "./foundation/graph-closure";
+export * from "./foundation/relation-ontology";
+export * from "./BLR-CP-001/prototype-contracts";
+export * from "./BLR-CP-001/prototype-generator";
+export * from "./BLR-CP-001/prototype-solver";
