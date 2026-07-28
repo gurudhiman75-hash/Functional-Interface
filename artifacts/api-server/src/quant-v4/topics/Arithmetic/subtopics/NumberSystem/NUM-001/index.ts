9cafc4dbd8b76e89239507d6ee9dc0900c8eb984
gurudhiman75-hash <gurudhiman75@gmail.com>
2026-07-28 20:42:37 +0530

    fix(number-system): export reviewed CP-003 runtime

--- artifacts/api-server/src/quant-v4/topics/Arithmetic/subtopics/NumberSystem/NUM-001/index.ts
+++ artifacts/api-server/src/quant-v4/topics/Arithmetic/subtopics/NumberSystem/NUM-001/index.ts
@@ -1,4 +1,4 @@
 export * from "./foundation/divisibility";
 export * from "./foundation/registry";
-export * from "./foundation/runtime";
+export * from "./foundation/runtime-reviewed";
 export * from "./foundation/types";
