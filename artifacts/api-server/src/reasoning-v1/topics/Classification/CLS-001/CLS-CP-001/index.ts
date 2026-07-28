db68f6f89063f0d73b08eb29e86a82a1f1badca2
gurudhiman75-hash <gurudhiman75@gmail.com>
2026-07-28 14:10:00 +0530

    feat(reasoning): export CLS-CP-001 discovery runtime

---
+++ artifacts/api-server/src/reasoning-v1/topics/Classification/CLS-001/CLS-CP-001/index.ts
@@ -0,0 +1,18 @@
+export {
+  generateClsCp001Prototype,
+  getClsCp001PrototypeDefinitions,
+  getClsCp001SemanticDataset,
+  independentlyVerifyClsCp001Question,
+} from "./runtime";
+export type {
+  AmbiguityAudit,
+  ClassificationTask,
+  Difficulty,
+  Explanation,
+  GeneratedClassificationQuestion,
+  PrototypeDefinition,
+  PrototypeFamily,
+  PrototypeId,
+  SemanticClass,
+  SemanticEntity,
+} from "./types";
