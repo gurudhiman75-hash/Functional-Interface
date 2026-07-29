12ef42f0edd6fa06208ba4e5c952e4c1fde7c031
gurudhiman75-hash <gurudhiman75@gmail.com>
2026-07-29 21:07:04 +0530

    feat(avg): export localized release API

--- artifacts/api-server/src/quant-v4/topics/Arithmetic/subtopics/Average/AVG-001/index.ts
+++ artifacts/api-server/src/quant-v4/topics/Arithmetic/subtopics/Average/AVG-001/index.ts
@@ -3,4 +3,5 @@ export * from "./foundation/library";
 export * from "./foundation/math";
 export * from "./foundation/pipeline";
 export * from "./foundation/editorial-v2-release";
+export * from "./foundation/localized-release";
 export * from "./question-studio-adapter";
