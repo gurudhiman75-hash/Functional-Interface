4a8018b5887e636197acc8ef2037fa32b21f3159
gurudhiman75-hash <gurudhiman75@gmail.com>
2026-07-29 11:13:50 +0530

    feat(reasoning): export CLS-CP-003 discovery runtime

---
+++ artifacts/api-server/src/reasoning-v1/topics/Classification/CLS-001/CLS-CP-003/index.ts
@@ -0,0 +1,33 @@
+export {
+  analyzeClsCp003Word,
+  auditClsCp003DisplayedJumbles,
+  auditClsCp003DisplayedWords,
+  generateClsCp003Prototype,
+  getClsCp003DatasetSummary,
+  getClsCp003PrototypeDefinitions,
+  independentlyVerifyClsCp003Question,
+} from "./runtime";
+export {
+  CLS_CP003_JUMBLE_WORDS,
+  CLS_CP003_PROTOTYPES,
+  CLS_CP003_WORDS,
+} from "./word-dataset.en";
+export type {
+  ClsCp003AmbiguityAudit,
+  ClsCp003AuditResult,
+  ClsCp003BoundaryClass,
+  ClsCp003Difficulty,
+  ClsCp003DifficultyFeatures,
+  ClsCp003Explanation,
+  ClsCp003GenerationProfile,
+  ClsCp003JumbleEntry,
+  ClsCp003PrototypeDefinition,
+  ClsCp003PrototypeId,
+  ClsCp003RepeatedTopology,
+  ClsCp003RuleId,
+  ClsCp003RuleSupport,
+  ClsCp003Task,
+  ClsCp003WordEntry,
+  ClsCp003WordFeatures,
+  GeneratedClsCp003Question,
+} from "./types";
