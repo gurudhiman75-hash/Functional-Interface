519aa364b8fc103f10896925f3161a90eee2b700
gurudhiman75-hash <gurudhiman75@gmail.com>
2026-08-08 11:30:30 +0530

    fix(admin): reuse canonical MathJax provider

--- artifacts/admin-app/src/main.tsx
+++ artifacts/admin-app/src/main.tsx
@@ -1,16 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
-import { AdminMathJax } from './components/shared/AdminMathJax';
+import { MathRenderingProvider } from './components/shared/MathRenderingProvider';
 import { ExamTreeAdminGate } from './integrations/ExamTreeAdminGate';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AdminMathJax>
+    <MathRenderingProvider>
       <ExamTreeAdminGate>
         <App />
       </ExamTreeAdminGate>
-    </AdminMathJax>
+    </MathRenderingProvider>
   </StrictMode>,
 );
