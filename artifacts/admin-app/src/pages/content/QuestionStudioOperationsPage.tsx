6b21b0e42f0612777cf86e70b2421016a617cc1c
gurudhiman75-hash <gurudhiman75@gmail.com>
2026-08-13 22:17:44 +0530

    refactor(probability): use standard Question Studio cockpit

--- artifacts/admin-app/src/pages/content/QuestionStudioOperationsPage.tsx
+++ artifacts/admin-app/src/pages/content/QuestionStudioOperationsPage.tsx
@@ -3,7 +3,6 @@ import { QuestionStudioCockpitPage } from './QuestionStudioCockpitPage';
 import { QuestionStudioDifficultyMixControls } from './QuestionStudioDifficultyMixControls';
 import { QuestionStudioExamProfileSummary } from './QuestionStudioExamProfileSummary';
 import { QuestionStudioInterestReviewPanel } from './QuestionStudioInterestReviewPanel';
-import { QuestionStudioProbabilityReviewPanel } from './QuestionStudioProbabilityReviewPanel';
 import { QuestionStudioProfileCalibration } from './QuestionStudioProfileCalibration';
 import { QuestionStudioReasoningReviewPanel } from './QuestionStudioReasoningReviewPanel';
 import { QuestionStudioRecoveryDock } from './QuestionStudioRecoveryDock';
@@ -12,7 +11,6 @@ import { QuestionStudioSeriesReviewPanel } from './QuestionStudioSeriesReviewPan
 export function QuestionStudioOperationsPage() {
   return (
     <>
-      <QuestionStudioProbabilityReviewPanel />
       <QuestionStudioInterestReviewPanel />
       <QuestionStudioSeriesReviewPanel />
       <QuestionStudioCalendarReviewPanel />
