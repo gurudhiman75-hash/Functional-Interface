2d7bf2c1ebc3b38472edb60cd8bd3200b60429d6
gurudhiman75-hash <gurudhiman75@gmail.com>
2026-08-19 17:09:20 +0530

    Load focused test runner mobile hardening styles

--- artifacts/examtree/src/main.tsx
+++ artifacts/examtree/src/main.tsx
@@ -2,5 +2,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./lib/install-safe-storage";
 import "./index.css";
+import "./test-runner-mobile.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
