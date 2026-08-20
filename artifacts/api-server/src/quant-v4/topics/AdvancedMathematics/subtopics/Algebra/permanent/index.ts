790370b026c63b8c1aa92be34659bc1bb17c4886
gurudhiman75-hash <gurudhiman75@gmail.com>
2026-08-20 10:35:22 +0530

    Algebra: export approved English V3 freeze

--- artifacts/api-server/src/quant-v4/topics/AdvancedMathematics/subtopics/Algebra/permanent/index.ts
+++ artifacts/api-server/src/quant-v4/topics/AdvancedMathematics/subtopics/Algebra/permanent/index.ts
@@ -1,6 +1,7 @@
 export * from "./allocation";
 export * from "./english-adapter";
 export * from "./english-freeze";
+export * from "./english-freeze-v3";
 export * from "./english-review-v2";
 export {
   ALG_ENGLISH_REVIEW_V3_AUTHORITY,
