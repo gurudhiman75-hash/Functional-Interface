196465b316e79f931a82a6701d00ca3361dc1db9
gurudhiman75-hash <gurudhiman75@gmail.com>
2026-08-20 08:34:30 +0530

    Algebra: expose normalized V3 review generator

--- artifacts/api-server/src/quant-v4/topics/AdvancedMathematics/subtopics/Algebra/permanent/index.ts
+++ artifacts/api-server/src/quant-v4/topics/AdvancedMathematics/subtopics/Algebra/permanent/index.ts
@@ -2,4 +2,9 @@ export * from "./allocation";
 export * from "./english-adapter";
 export * from "./english-freeze";
 export * from "./english-review-v2";
-export * from "./english-review-v3";
+export {
+  ALG_ENGLISH_REVIEW_V3_AUTHORITY,
+  ALG_ENGLISH_REVIEW_V3_ID,
+  type AlgPermanentEnglishReviewV3Item,
+} from "./english-review-v3";
+export { generateAlgPermanentEnglishReviewV3 } from "./english-review-v3-render";
