0dc934f64206c5d9ba9a84061694a98e3ea898ee
gurudhiman75-hash <gurudhiman75@gmail.com>
2026-08-30 11:25:38 +0530

    Frontend: add Performance to student sidebar

--- artifacts/examtree/src/components/AppSidebar.tsx
+++ artifacts/examtree/src/components/AppSidebar.tsx
@@ -1,5 +1,6 @@
 import { Link, useLocation } from "wouter";
 import {
+  BarChart3,
   Bookmark,
   ClipboardList,
   Home,
@@ -34,6 +35,7 @@ const primaryLinks = [
   { href: "/store", label: "Store", icon: ShoppingBag },
   { href: "/my-packages", label: "My Purchases", icon: ReceiptText },
   { href: "/dashboard", label: "My Activity", icon: Target },
+  { href: "/performance", label: "Performance", icon: BarChart3 },
   { href: "/bookmarks", label: "Bookmarks", icon: Bookmark },
 ];
 
