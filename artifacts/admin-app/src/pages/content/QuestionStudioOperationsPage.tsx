d690c10d82b984ed8eef14d431860236cd0bdf7a
gurudhiman75-hash <gurudhiman75@gmail.com>
2026-09-06 11:44:11 +0530

    integrate(computer): merge engine-aware admin cockpit controls

--- artifacts/admin-app/src/pages/content/QuestionStudioOperationsPage.tsx
+++ artifacts/admin-app/src/pages/content/QuestionStudioOperationsPage.tsx
@@ -1,6 +1,9 @@
 import { QuestionStudioAlgebraReviewPanel } from './QuestionStudioAlgebraReviewPanel';
 import { QuestionStudioCalendarReviewPanel } from './QuestionStudioCalendarReviewPanel';
 import { QuestionStudioCockpitPage } from './QuestionStudioCockpitPage';
+import { QuestionStudioCom003PreviewPanel } from './QuestionStudioCom003PreviewPanel';
+import { QuestionStudioCom003ReviewPanel } from './QuestionStudioCom003ReviewPanel';
+import { QuestionStudioComputerAwarenessReviewPanel } from './QuestionStudioComputerAwarenessReviewPanel';
 import { QuestionStudioCubesDiceReviewPanel } from './QuestionStudioCubesDiceReviewPanel';
 import { QuestionStudioDataSufficiencyReviewPanel } from './QuestionStudioDataSufficiencyReviewPanel';
 import { QuestionStudioDifficultyMixControls } from './QuestionStudioDifficultyMixControls';
@@ -16,6 +19,9 @@ import { QuestionStudioStatementAssumptionReviewPanel } from './QuestionStudioSt
 export function QuestionStudioOperationsPage() {
   return (
     <>
+      <QuestionStudioComputerAwarenessReviewPanel />
+      <QuestionStudioCom003PreviewPanel />
+      <QuestionStudioCom003ReviewPanel />
       <QuestionStudioDataSufficiencyReviewPanel />
       <QuestionStudioAlgebraReviewPanel />
       <QuestionStudioSpatialReviewPanel />
