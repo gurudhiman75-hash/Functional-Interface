0c6fe48dee6edfc3e4847709f7d060c2603d31d2
gurudhiman75-hash <gurudhiman75@gmail.com>
2026-09-11 19:39:22 +0530

    feat(english): extend shared types for CP004 pronouns

--- artifacts/api-server/src/english-v1/core/types.ts
+++ artifacts/api-server/src/english-v1/core/types.ts
@@ -8,7 +8,8 @@ export type Eng001QlId =
 export type Eng001CpId =
   | "ENG-001-CP001"
   | "ENG-001-CP002"
-  | "ENG-001-CP003";
+  | "ENG-001-CP003"
+  | "ENG-001-CP004";
 
 export type SvaRuleId =
   | "GR-SVA-001"
@@ -46,7 +47,19 @@ export type ArticleRuleId =
   | "GR-ART-009"
   | "GR-ART-010";
 
-export type GrammarRuleId = SvaRuleId | TenseRuleId | ArticleRuleId;
+export type PronounRuleId =
+  | "GR-PRN-001"
+  | "GR-PRN-002"
+  | "GR-PRN-003"
+  | "GR-PRN-004"
+  | "GR-PRN-005"
+  | "GR-PRN-006"
+  | "GR-PRN-007"
+  | "GR-PRN-008"
+  | "GR-PRN-009"
+  | "GR-PRN-010";
+
+export type GrammarRuleId = SvaRuleId | TenseRuleId | ArticleRuleId | PronounRuleId;
 
 export type SvaMutationId =
   | "MUT-SVA-NUMBER-001"
@@ -84,7 +97,19 @@ export type ArticleMutationId =
   | "MUT-ART-COUNTABILITY-001"
   | "MUT-ART-DETERMINER-NUMBER-001";
 
-export type GrammarMutationId = SvaMutationId | TenseMutationId | ArticleMutationId;
+export type PronounMutationId =
+  | "MUT-PRN-SUBJECT-CASE-001"
+  | "MUT-PRN-OBJECT-CASE-001"
+  | "MUT-PRN-POSSESSIVE-FORM-001"
+  | "MUT-PRN-REFLEXIVE-COREFERENCE-001"
+  | "MUT-PRN-REFLEXIVE-MISUSE-001"
+  | "MUT-PRN-ANTECEDENT-NUMBER-001"
+  | "MUT-PRN-WHO-WHOM-001"
+  | "MUT-PRN-RELATIVE-PERSON-THING-001"
+  | "MUT-PRN-DEMONSTRATIVE-NUMBER-001"
+  | "MUT-PRN-WHOSE-WHOS-001";
+
+export type GrammarMutationId = SvaMutationId | TenseMutationId | ArticleMutationId | PronounMutationId;
 
 export interface DifficultyDimensions {
   ruleComplexity: 1 | 2 | 3 | 4 | 5;
@@ -97,7 +122,7 @@ export interface DifficultyDimensions {
 
 export interface EnglishGrammarRule {
   ruleId: GrammarRuleId;
-  category: "subject_verb_agreement" | "tenses_sequence" | "articles_determiners";
+  category: "subject_verb_agreement" | "tenses_sequence" | "articles_determiners" | "pronouns";
   name: string;
   principle: string;
   mutationId: GrammarMutationId;
