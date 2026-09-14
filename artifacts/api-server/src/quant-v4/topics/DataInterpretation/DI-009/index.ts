21b97c052ed4e493cd4bb5aaf3cd7cace1bd1531
gurudhiman75-hash <gurudhiman75@gmail.com>
2026-09-14 19:41:29 +0530

    feat(di): export DI-009 promotion surfaces

--- artifacts/api-server/src/quant-v4/topics/DataInterpretation/DI-009/index.ts
+++ artifacts/api-server/src/quant-v4/topics/DataInterpretation/DI-009/index.ts
@@ -1,3 +1,6 @@
 export * from "./types";
 export * from "./histogram-set";
 export * from "./independent-verifier";
+export * from "./permanent-ql-registry";
+export * from "./permanent-question-generator";
+export * from "./question-studio-adapter";
