068da1be8731ef2cb9f9224816db280635bd0718
gurudhiman75-hash <gurudhiman75@gmail.com>
2026-09-16 08:30:42 +0530

    COA CP002: export Editorial V2 authority

--- artifacts/api-server/src/reasoning-v1/topics/Course-of-Action/COA-001/index.ts
+++ artifacts/api-server/src/reasoning-v1/topics/Course-of-Action/COA-001/index.ts
@@ -3,5 +3,6 @@ export * from "./action-validity-model.ts";
 export * from "./cp001-english-authorities.ts";
 export * from "./cp001-editorial-v2.ts";
 export * from "./cp002-direct-preventive-expansion.ts";
+export * from "./cp002-editorial-v2.ts";
 export * from "./english-authorities.ts";
 export * from "./cp002-generator.ts";
