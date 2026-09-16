3316798b03040a14e16132225682cf9fe621e332
gurudhiman75-hash <gurudhiman75@gmail.com>
2026-09-16 15:07:51 +0530

    Add PGK CP018 QL124 metadata

---
+++ artifacts/api-server/src/knowledge-v1/punjab-gk/pgk-001-cp018-ql124.ts
@@ -0,0 +1,5 @@
+export const PGK_001_CP018_QL124 = Object.freeze({
+  id: "PGK-001-QL-124",
+  focus: "Territorial outcomes of 1966",
+  reviewQuestionIds: Object.freeze(["PGK-001-CP018-Q031","PGK-001-CP018-Q032","PGK-001-CP018-Q033","PGK-001-CP018-Q034","PGK-001-CP018-Q035","PGK-001-CP018-Q036"]),
+});
