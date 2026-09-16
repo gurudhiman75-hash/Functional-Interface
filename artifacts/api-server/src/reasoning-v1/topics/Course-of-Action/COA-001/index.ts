d4e863694fcd2e57d4c91305e081e8e751d6fae2
gurudhiman75-hash <gurudhiman75@gmail.com>
2026-09-16 08:29:06 +0530

    COA CP002: export current English expansion runtime

--- artifacts/api-server/src/reasoning-v1/topics/Course-of-Action/COA-001/index.ts
+++ artifacts/api-server/src/reasoning-v1/topics/Course-of-Action/COA-001/index.ts
@@ -2,3 +2,6 @@ export * from "./types.ts";
 export * from "./action-validity-model.ts";
 export * from "./cp001-english-authorities.ts";
 export * from "./cp001-editorial-v2.ts";
+export * from "./cp002-direct-preventive-expansion.ts";
+export * from "./english-authorities.ts";
+export * from "./cp002-generator.ts";
