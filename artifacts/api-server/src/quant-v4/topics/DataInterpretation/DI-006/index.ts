6156c54f11188e8b5d588bcc5299ea1c0249babb
gurudhiman75-hash <gurudhiman75@gmail.com>
2026-09-18 08:52:14 +0530

    feat(di): export DI-006 V2 review engine

--- artifacts/api-server/src/quant-v4/topics/DataInterpretation/DI-006/index.ts
+++ artifacts/api-server/src/quant-v4/topics/DataInterpretation/DI-006/index.ts
@@ -1,3 +1,6 @@
 export * from "./types";
 export * from "./caselet-set";
 export * from "./independent-verifier";
+export * from "./caselet-v2-types";
+export * from "./caselet-set-v2";
+export * from "./independent-verifier-v2";
