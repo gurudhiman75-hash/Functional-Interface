d7a35a16a596a23f1407c2a9b81425fe95359655
gurudhiman75-hash <gurudhiman75@gmail.com>
2026-09-18 19:08:18 +0530

    feat(di): export DI-006 V2 controlled review

--- artifacts/api-server/src/quant-v4/topics/DataInterpretation/DI-006/index.ts
+++ artifacts/api-server/src/quant-v4/topics/DataInterpretation/DI-006/index.ts
@@ -1,3 +1,4 @@
 export * from "./types";
 export * from "./caselet-set";
 export * from "./independent-verifier";
+export * from "./caselet-v2-types";\nexport * from "./caselet-set-v2";\nexport * from "./independent-verifier-v2";\nexport * from "./permanent-ql-registry";\nexport * from "./permanent-question-generator";\nexport * from "./question-studio-adapter";\n
