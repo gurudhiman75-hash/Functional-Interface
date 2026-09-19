330bc7f1ab4356ee137f439e9c5c73b694a93d5e
gurudhiman75-hash <gurudhiman75@gmail.com>
2026-09-19 08:11:27 +0530

    Export DI-008 V2 review engine

--- artifacts/api-server/src/quant-v4/topics/DataInterpretation/DI-008/index.ts
+++ artifacts/api-server/src/quant-v4/topics/DataInterpretation/DI-008/index.ts
@@ -1,3 +1,6 @@
 export * from "./types";
 export * from "./arithmetic-set";
 export * from "./independent-verifier";
+export * from "./arithmetic-v2-types";
+export * from "./arithmetic-set-v2";
+export * from "./independent-verifier-v2";
